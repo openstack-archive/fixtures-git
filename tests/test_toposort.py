9973609df43bbc42d261b422d6b61124c3c6a282
Darragh Bailey <daragh.bailey@gmail.com>
2018-03-10 11:17:45 +0000

    Initial fixture for git
    Provide fixture object that can be used to build isolated test envs
    when working with git and developing git tooling that uses the git
    application.
    
    Some basic tests on the topological sorting to ensure nodes are
    returned in an expected order without requiring the code to enforce the
    order.
    
    Change-Id: I5b39f8d52beca848adb20d646c26d91c4b977b92

---
+++ tests/test_toposort.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2018 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import testtools
+
+import base
+import fixtures_git
+
+
+class TestResolve(testtools.TestCase):
+
+    def test_ordered(self):
+        nodes = [
+            ('A', []),
+            ('B', ['A']),
+            ('C', ['B']),
+        ]
+        self.assertEquals(
+            nodes,
+            list(fixtures_git.reverse_toposort(nodes)),
+        )
+
+    def test_unordered(self):
+        nodes = [
+            ('B', ['A']),
+            ('C', ['B']),
+            ('A', []),
+        ]
+        self.assertEquals(
+            [('A', []), ('B', ['A']), ('C', ['B'])],
+            list(fixtures_git.reverse_toposort(nodes))
+        )
+
+    def test_merge(self):
+        nodes = [
+            ('B', ['A']),
+            ('C', ['B']),
+            ('A', []),
+            ('E', ['D', 'C']),
+            ('D', ['A']),
+        ]
+        sorted = list(fixtures_git.reverse_toposort(nodes))
+        self.assertThat(
+            (nodes[2], nodes[0], nodes[1], nodes[3]),
+            base.IsOrderedSubsetOf(sorted)
+        )
+        self.assertThat(
+            (nodes[2], nodes[4], nodes[3]),
+            base.IsOrderedSubsetOf(sorted)
+        )
+
+    def test_multiple_merges(self):
+        nodes = [
+            ('B', ['A']),
+            ('C', ['B']),
+            ('A', []),
+            ('E', ['D', 'C']),
+            ('D', ['A']),
+            ('G', ['F', 'C']),
+            ('F', ['A']),
+        ]
+        sorted = list(fixtures_git.reverse_toposort(nodes))
+        # A -> B -> C -> D -> E
+        self.assertThat(
+            (nodes[2], nodes[0], nodes[1], nodes[4], nodes[3]),
+            base.IsOrderedSubsetOf(sorted)
+        )
+        # A -> F -> G
+        self.assertThat(
+            (nodes[2], nodes[6], nodes[5]),
+            base.IsOrderedSubsetOf(sorted)
+        )
+        # A -> B -> C -> G
+        self.assertThat(
+            (nodes[2], nodes[0], nodes[1], nodes[5]),
+            base.IsOrderedSubsetOf(sorted)
+        )
+
+    def test_merge_multiple_roots(self):
+        nodes = [
+            ('B', ['A']),
+            ('C', []),  # root commit
+            ('A', []),  # root commit
+            ('D', ['B', 'C']),
+        ]
+        sorted = list(fixtures_git.reverse_toposort(nodes))
+        # assert partial ordering because nodes A & B may come
+        # before or after node C. Just make sure that node D
+        # is defined after them.
+        self.assertThat(
+            (nodes[2], nodes[0], nodes[3]),
+            base.IsOrderedSubsetOf(sorted),
+        )
+        self.assertThat(
+            (nodes[1], nodes[3]),
+            base.IsOrderedSubsetOf(sorted),
+        )
+
